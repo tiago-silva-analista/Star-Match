72816d11e05ec6cfed69cc16564c529711b875d0
tiago-silva-analista <tiago.silva.analista@gmail.com>
2020-05-16 17:25:50 -0300

    Bubild files

---
+++ build/precache-manifest.6fefa74f165c880eed9848d998ec3b6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4adbdf810b03b0aea9e8d08b96c9e24b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "404f5e7d94c5f44eee38",
+    "url": "/static/css/main.95f43fd3.chunk.css"
+  },
+  {
+    "revision": "fe4d61786f3a847faaac",
+    "url": "/static/js/2.59fd0774.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.59fd0774.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "404f5e7d94c5f44eee38",
+    "url": "/static/js/main.448e91ac.chunk.js"
+  },
+  {
+    "revision": "d3829789bb0379428de0",
+    "url": "/static/js/runtime-main.356a64ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
